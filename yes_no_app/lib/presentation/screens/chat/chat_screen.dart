b73dc343d2fb3279541125ca8cb96197b4ba4848
Facundo <faccu.veron92@gmail.com>
2023-08-08 00:31:32 -0300

    Se crearon las screens de los usuarios.

---
+++ yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://addons-media.operacdn.com/media/CACHE/images/themes/45/131845/1.0-rev1/images/7cf6db01-a5f7-4107-acf0-47651d14ab03/50d5f7da54bfb2b13ac648ce67416ad4.jpg'),
+          ),
+        ),
+        title: const Text('Scarlett Johansson'),
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return (index % 2 == 0)
+                    ? const HerMessageBubble()
+                    : const MyMessageBubble();
+              },
+            )),
+            const MessageFieldBox()
+          ],
+        ),
+      ),
+    );
+  }
+}
